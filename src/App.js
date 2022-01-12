4e428c4a9840de0bff436a9cfb8c1b44c5040826
7ORP3DO <davisudha806@gmail.com>
2022-01-12 18:16:05 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+import About from './Components/About';
+
+
+var myName = 'Prateek';
+
+function App() {
+  return (
+    <>
+      <Navbar title="React App"/>
+      <About/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function About(prop) {
+    return (
+        <div>
+            <h2>About Us</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores incidunt fuga recusandae? Voluptas non suscipit totam cum repudiandae quae aliquid, Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam beatae omnis amet temporibus soluta tempore ratione aspernatur et cupiditate provident quaerat, ea, nulla nihil deleniti aut repellendus minima magni! Debitis? ullam corrupti vero, natus voluptatum corporis eligendi nam magni quaerat!</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est quam iure consequatur aliquam quasi architecto repellat dolorum, quaerat voluptate vitae.</p>
+        </div>
+    )
+}
